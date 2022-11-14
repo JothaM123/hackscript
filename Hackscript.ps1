f11604f9e940eae579f26debca50dda136cbd3de
Jotham <jothamsamuel05@gmail.com>
2022-11-14 16:37:58 +0530

    Add files via upload

---
+++ Hackscript.ps1
@@ -0,0 +1,14 @@
+﻿echo "Hackscript"
+$command = Read-Host -Prompt "<H> : > "
+
+if ($command -eq "hack_wifi_psw()"){
+    $name = Read-Host -Prompt "Enter Connection Name - "
+    echo `n
+    netsh wlan show profile $name key=clear | findstr "Key Content" 
+    echo `n
+}
+
+if ($command -eq "hack_help()"){
+    echo "1. hack_wifi_psw()`n2. hack_help()"
+    echo `n
+}
